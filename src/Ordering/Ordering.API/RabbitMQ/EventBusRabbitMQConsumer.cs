ecda0d165ed7de31209a426796569a660a149947
Ahmed Wagdy <ahmedoneplusone@gmail.com>
2022-06-02 10:26:57 +0200

    Fixed catalog api folder rename for good this time

---
+++ src/Ordering/Ordering.API/RabbitMQ/EventBusRabbitMQConsumer.cs
@@ -0,0 +1,55 @@
+﻿using AutoMapper;
+using EventBusRabbitMQ.Common;
+using Events;
+using MediatR;
+using Newtonsoft.Json;
+using Ordering.Application.Commands;
+using Ordering.Core.Repositories;
+using RabbitMQ.Client.Events;
+using src.Common.EventBusRabbitMQ;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.API.RabbitMQ
+{
+    public class EventBusRabbitMQConsumer
+    {
+        private readonly IRabbitMQConnection _connection;
+        private readonly IMediator _mediator;
+        private readonly IMapper _mapper;
+        private readonly IOrderRepo _repo;
+
+        public EventBusRabbitMQConsumer(IRabbitMQConnection connection, IMediator mediator, IMapper mapper, IOrderRepo repo)
+        {
+            _connection = connection ?? throw new ArgumentNullException(nameof(connection));
+            _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            _repo = repo ?? throw new ArgumentNullException(nameof(repo));
+        }
+
+        public void Consume()
+        {
+            var channel = _connection.CreateModel();
+            channel.QueueDeclare(queue: EventBusConstants.BasketCheckoutQueue, durable: false, exclusive: false, autoDelete: false, arguments: null);
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += ReceivedEvent;
+            channel.BasicConsume(queue: EventBusConstants.BasketCheckoutQueue, autoAck: true, consumer: consumer,noLocal:false,exclusive:false,arguments:null,consumerTag:"");
+
+        }
+
+        private async void ReceivedEvent(object sender,BasicDeliverEventArgs e)
+        {
+            if(e.RoutingKey == EventBusConstants.BasketCheckoutQueue)
+            {
+                var message = Encoding.UTF8.GetString(e.Body.Span);
+                var basketCheckoutEvent = JsonConvert.DeserializeObject<BasketCheckoutEvent>(message);
+
+                var command = _mapper.Map<CheckoutOrderCommand>(basketCheckoutEvent);
+
+            }
+        }
+    }
+}
